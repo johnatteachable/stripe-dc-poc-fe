3782335ac67a44c446086a1fbb33c801f840a29d
John T Scherer <john.scherer@teachable.com>
2025-07-17 13:19:05 -0400

    first commit

---
+++ backup/index copy.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {loadStripe} from '@stripe/stripe-js';
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout
+} from '@stripe/react-stripe-js';
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx'
+);
+
+const App = ({clientSecret}) => {
+  const options = {clientSecret};
+  return (
+    <EmbeddedCheckoutProvider
+      stripe={stripePromise}
+      options={options}
+    >
+      <EmbeddedCheckout />
+    </EmbeddedCheckoutProvider>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState,useEffect } from 'react';
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout
+} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
 function App() {
+  const [clientSecret, setClientSecret] = useState('')
+
+  const getPaymentIntent = () => {
+    console.log('Clicked checkout');
+
+    return fetch('http://localhost:8080/create-checkout-session', {
+      method: 'POST',
+      headers: {
+      Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: 'value',
+      }),
+    });
+  }
+
+  const checkPing = () => {
+    console.log('Clicked ping')
+    fetch('http://localhost:8080/ping', {
+      method: 'GET',
+    });
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+       const data = await getPaymentIntent();
+       const jsondata = await data.json();
+       console.log("Printing jsondata.client_secret");
+       console.log(jsondata);
+       setClientSecret(jsondata);
+    }
+    fetchData();
+  }, []);
+
+  const stripePromise = loadStripe("pk_test_t8gwupmepWNtJUkHaRmC3Tlk");
+  const options = {clientSecret};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EmbeddedCheckoutProvider
+        stripe={stripePromise}
+        options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
     </div>
+
   );
 }
 
